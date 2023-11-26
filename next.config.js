4bfc7aeddb258bde444f812e0d06bd55d7d3469b
chabonhamay4so9 <93881933+chabonhamay4so9@users.noreply.github.com>
2023-11-26 21:49:48 +0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    url:"https://vnexpress.net/linh-nga-dan-bo-gay-nhieu-uav-len-noc-thiet-giap-4680834.html"
+  },
+}
+
+module.exports = nextConfig
